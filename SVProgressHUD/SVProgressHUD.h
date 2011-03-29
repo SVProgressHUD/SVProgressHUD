06faacf839b20b148bd04a94b9ab4501adcf6974
Sam Vermette <samvermette@gmail.com>
2011-03-28 22:13:06 -0400

    Added some comments/help to interface

--- SVProgressHUD/SVProgressHUD.h
+++ SVProgressHUD/SVProgressHUD.h
@@ -12,16 +12,23 @@
 	NSTimer *fadeOutTimer;
 }
 
-+ (void)show; // HUD gets added to UIApplication's keyWindow, which doesn't reflect the current device orientation
+/* 
+showInView:(UIView*)	-> the view you're adding the HUD to. By default, it's added to UIApplication's keyWindow, which doesn't reflect the current device orientation.
+status:(NSString*)		-> a loading status for the HUD (different from the success and error messages)
+networkIndicator:(BOOL)	-> whether or not the HUD also triggers the UIApplication's network activity indicator (default is YES)
+posY:(CGFloat)			-> the vertical position of the HUD (default is (viewHeight/2)-50)
+*/
+ 
++ (void)show;
 + (void)showInView:(UIView*)view;
 + (void)showInView:(UIView*)view status:(NSString*)string;
 + (void)showInView:(UIView*)view status:(NSString*)string networkIndicator:(BOOL)show;
 + (void)showInView:(UIView*)view status:(NSString*)string networkIndicator:(BOOL)show posY:(CGFloat)posY;
 
-+ (void)setStatus:(NSString*)string;
++ (void)setStatus:(NSString*)string; // change the HUD loading status while it's showing
 
-+ (void)dismiss;
-+ (void)dismissWithSuccess:(NSString*)successString;
-+ (void)dismissWithError:(NSString*)errorString;
++ (void)dismiss; // simply dismiss the HUD with a fade+scale out animation
++ (void)dismissWithSuccess:(NSString*)successString; // also displays the success icon image
++ (void)dismissWithError:(NSString*)errorString; // also displays the error icon image
 
 @end
