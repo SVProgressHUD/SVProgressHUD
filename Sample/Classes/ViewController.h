22b87b66dd1469a2477a63e60fcd31fae6f7c9b8
gcamp <guillaume.campagna@gmail.com>
2011-04-05 16:39:56 -0400

    Remove useless code

--- Sample/Classes/ViewController.h
+++ Sample/Classes/ViewController.h
@@ -8,9 +8,7 @@
 
 #import <UIKit/UIKit.h>
 
-@interface ViewController : UIViewController {
-
-}
+@interface ViewController : UIViewController
 
 - (IBAction)show;
 - (IBAction)showWithStatus;
--- Sample/Classes/ViewController.m
+++ Sample/Classes/ViewController.m
@@ -37,23 +37,4 @@
 	[SVProgressHUD dismissWithError:@"Failed with Error"];
 }
 
-#pragma mark -
-
-- (void)didReceiveMemoryWarning {
-	// Releases the view if it doesn't have a superview.
-    [super didReceiveMemoryWarning];
-	
-	// Release any cached data, images, etc that aren't in use.
-}
-
-- (void)viewDidUnload {
-	// Release any retained subviews of the main view.
-	// e.g. self.myOutlet = nil;
-}
-
-
-- (void)dealloc {
-    [super dealloc];
-}
-
 @end
