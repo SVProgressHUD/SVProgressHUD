6e9e4571229ca3b49f0a4be066603f01f1880277
gcamp <guillaume.campagna@gmail.com>
2011-04-08 21:02:52 -0400

    Add modal in the demo

--- Sample/Classes/ViewController.m
+++ Sample/Classes/ViewController.m
@@ -11,15 +11,19 @@
 
 @implementation ViewController
 
+@synthesize modalSwitch;
+
 #pragma mark -
 #pragma mark Show Methods Sample
 
 - (void)show {
 	[SVProgressHUD showInView:self.view];
+    if (modalSwitch.on) [self performSelector:@selector(dismiss) withObject:nil afterDelay:5.0];
 }
 
 - (void)showWithStatus {
 	[SVProgressHUD showInView:self.view status:@"Doing Stuff"];
+    if (modalSwitch.on) [self performSelector:@selector(dismiss) withObject:nil afterDelay:5.0];
 }
 
 #pragma mark -
@@ -37,4 +41,16 @@
 	[SVProgressHUD dismissWithError:@"Failed with Error"];
 }
 
+- (IBAction)modalChanged:(UISwitch*)sender {
+    [SVProgressHUD setModal:sender.on];
+}
+
+- (void)dealloc {
+    [modalSwitch release];
+    [super dealloc];
+}
+- (void)viewDidUnload {
+    [self setModalSwitch:nil];
+    [super viewDidUnload];
+}
 @end
