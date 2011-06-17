2508ba566f16a910efed6703efd89dbf64f03146
Sam Vermette <sam@Sam-Vermettes-iMac.local>
2011-06-17 18:10:01 -0400

    Use the rootViewController if possible to reflect the device orientation (thanks Guillaume Campagna).

--- SVProgressHUD/SVProgressHUD.h
+++ SVProgressHUD/SVProgressHUD.h
@@ -8,7 +8,7 @@
 @interface SVProgressHUD : UIView 
 
 /* 
-showInView:(UIView*)	-> the view you're adding the HUD to. By default, it's added to UIApplication's keyWindow, which doesn't reflect the current device orientation.
+showInView:(UIView*)	-> the view you're adding the HUD to. By default, it's added to the keyWindow rootViewController, or the keyWindow if the rootViewController is nil
 status:(NSString*)		-> a loading status for the HUD (different from the success and error messages)
 networkIndicator:(BOOL)	-> whether or not the HUD also triggers the UIApplication's network activity indicator (default is YES)
 posY:(CGFloat)			-> the vertical position of the HUD (default is (viewHeight/2)-50)
