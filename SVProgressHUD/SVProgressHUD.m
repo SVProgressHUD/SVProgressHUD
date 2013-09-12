7bd14a7222485e1e513cdcadde9aa4d90544438a
Marc Fernandez <marc.fernandezg@gmail.com>
2013-09-12 14:23:27 -0700

    Fix issue with iOS7 and status bar

--- SVProgressHUD/SVProgressHUD.m
+++ SVProgressHUD/SVProgressHUD.m
@@ -530,6 +530,11 @@ CGFloat SVProgressHUDRingThickness = 6;
                                                                                  object:nil
                                                                                userInfo:notificationUserInfo];
                              
+                             // Tell the rootViewController to update the StatusBar appearance
+                             UIViewController *rootController = UIApplication.sharedApplication.delegate.window.rootViewController;
+                             if ([rootController respondsToSelector:@selector(setNeedsStatusBarAppearanceUpdate)]) {
+                               [rootController setNeedsStatusBarAppearanceUpdate];
+                             }
                              // uncomment to make sure UIWindow is gone from app.windows
                              //NSLog(@"%@", [UIApplication sharedApplication].windows);
                              //NSLog(@"keyWindow = %@", [UIApplication sharedApplication].keyWindow);
