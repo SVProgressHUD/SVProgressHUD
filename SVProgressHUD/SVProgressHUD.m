cc2a700e1cc6eda0e390ed9a9b43eadd47153796
Aleksei Shevchenko <i.am.skeeet@gmail.com>
2014-02-19 23:34:26 -0800

    Fixed build errors when target version <7.0

--- SVProgressHUD/SVProgressHUD.m
+++ SVProgressHUD/SVProgressHUD.m
@@ -745,6 +745,7 @@ static const CGFloat SVProgressHUDRingThickness = 6;
         _hudView.autoresizingMask = (UIViewAutoresizingFlexibleBottomMargin | UIViewAutoresizingFlexibleTopMargin |
                                      UIViewAutoresizingFlexibleRightMargin | UIViewAutoresizingFlexibleLeftMargin);
         
+#if __IPHONE_OS_VERSION_MIN_REQUIRED >= 70000
         UIInterpolatingMotionEffect *effectX = [[UIInterpolatingMotionEffect alloc] initWithKeyPath: @"center.x" type: UIInterpolatingMotionEffectTypeTiltAlongHorizontalAxis];
         effectX.minimumRelativeValue = @(-SVProgressHUDParallaxDepthPoints);
         effectX.maximumRelativeValue = @(SVProgressHUDParallaxDepthPoints);
@@ -755,7 +756,7 @@ static const CGFloat SVProgressHUDRingThickness = 6;
         
         [_hudView addMotionEffect: effectX];
         [_hudView addMotionEffect: effectY];
-        
+#endif
         [self addSubview:_hudView];
     }
     return _hudView;
