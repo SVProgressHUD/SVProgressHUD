f83e1c1672a29e1938c8c7b3f7712ed332775181
BenMaer <ben@resplendent.co>
2014-09-20 14:36:39 -0400

    Gave animationDuration an initial value of 0 to avoid warning in newer Xcode versions.

--- SVProgressHUD/SVProgressHUD.m
+++ SVProgressHUD/SVProgressHUD.m
@@ -398,7 +398,7 @@ static const CGFloat SVProgressHUDParallaxDepthPoints = 10;
 - (void)positionHUD:(NSNotification*)notification {
     
     CGFloat keyboardHeight;
-    double animationDuration;
+    double animationDuration = 0;
     
     UIInterfaceOrientation orientation = [[UIApplication sharedApplication] statusBarOrientation];
     // no transforms applied to window in iOS 8
