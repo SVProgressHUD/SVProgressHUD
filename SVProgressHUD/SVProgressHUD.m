c7ebc29b1644686f4740476928f6f93e3dd2b4ae
edwardmp <e.poot@evolutionhost.nl>
2014-10-03 17:16:02 +0200

    Macro now checks deployment target instead of base SDK

--- SVProgressHUD/SVProgressHUD.m
+++ SVProgressHUD/SVProgressHUD.m
@@ -276,20 +276,17 @@ static const CGFloat SVProgressHUDParallaxDepthPoints = 10;
     if(string) {
         CGSize constraintSize = CGSizeMake(200, 300);
         CGRect stringRect;
-        if ([string respondsToSelector:@selector(boundingRectWithSize:options:attributes:context:)]) {
-          stringRect = [string boundingRectWithSize:constraintSize
-                                            options:(NSStringDrawingUsesFontLeading|NSStringDrawingTruncatesLastVisibleLine|NSStringDrawingUsesLineFragmentOrigin)
-                                         attributes:@{NSFontAttributeName: self.stringLabel.font}
-                                            context:NULL];
-        } else {
+        #if __IPHONE_OS_VERSION_MIN_REQUIRED >= 70000
+            stringRect = [string boundingRectWithSize:constraintSize
+                                                  options:(NSStringDrawingUsesFontLeading|NSStringDrawingTruncatesLastVisibleLine|NSStringDrawingUsesLineFragmentOrigin)
+                                               attributes:@{NSFontAttributeName: self.stringLabel.font}
+                                                  context:NULL];
+        #else
             CGSize stringSize;
-            #ifdef __IPHONE_8_0
-                stringSize = [string sizeWithAttributes:@{NSFontAttributeName:[UIFont fontWithName:self.stringLabel.font.fontName size:self.stringLabel.font.pointSize]}];
-            #else
-                stringSize = [string sizeWithFont:self.stringLabel.font constrainedToSize:CGSizeMake(200, 300)];
-            #endif
+            stringSize = [string sizeWithFont:self.stringLabel.font constrainedToSize:CGSizeMake(200, 300)];
             stringRect = CGRectMake(0.0f, 0.0f, stringSize.width, stringSize.height);
-        }
+        #endif
+        
         stringWidth = stringRect.size.width;
         stringHeight = ceil(stringRect.size.height);
         
