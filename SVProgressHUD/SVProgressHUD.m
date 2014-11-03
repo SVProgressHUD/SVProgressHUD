51e46db9051faf64a03098f8d5eeb629b557f4cc
Yozone Wong <wangyaozh@gmail.com>
2014-11-03 15:47:27 +0800

    fixed bug #359

--- SVProgressHUD/SVProgressHUD.m
+++ SVProgressHUD/SVProgressHUD.m
@@ -308,7 +308,8 @@ static const CGFloat SVProgressHUDParallaxDepthPoints = 10;
             hudWidth+=24;
         } else {
             hudWidth+=24;
-            labelRect = CGRectMake(0, labelRectY, hudWidth, stringHeight);
+            CGFloat labelRectX = (hudWidth - stringWidth) / 2;
+            labelRect = CGRectMake(labelRectX, labelRectY, stringWidth, stringHeight);
         }
     }
 	
