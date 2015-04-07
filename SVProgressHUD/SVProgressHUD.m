5476a34ac939d260f556116a46d4c89d1fce790d
Kévin Renella <kevin.renella@gmail.com>
2015-04-08 01:35:44 +0200

    Fix warnings using flag CLANG_WARN_OBJC_IMPLICIT_RETAIN_SELF

--- SVProgressHUD/SVProgressHUD.m
+++ SVProgressHUD/SVProgressHUD.m
@@ -774,14 +774,14 @@ static const CGFloat SVProgressHUDUndefinedProgress = -1;
                              
                              [[NSNotificationCenter defaultCenter] removeObserver:self];
                              [self cancelRingLayerAnimation];
-                             [_hudView removeFromSuperview];
-                             _hudView = nil;
+                             [self->_hudView removeFromSuperview];
+                             self->_hudView = nil;
                              
-                             [_overlayView removeFromSuperview];
-                             _overlayView = nil;
+                             [self->_overlayView removeFromSuperview];
+                             self->_overlayView = nil;
                              
-                             [_indefiniteAnimatedView removeFromSuperview];
-                             _indefiniteAnimatedView = nil;
+                             [self->_indefiniteAnimatedView removeFromSuperview];
+                             self->_indefiniteAnimatedView = nil;
                              
                              UIAccessibilityPostNotification(UIAccessibilityScreenChangedNotification, nil);
                              
