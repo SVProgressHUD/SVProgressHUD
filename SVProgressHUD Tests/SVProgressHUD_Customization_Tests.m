67de06e22cd5e140130378c251f5a8758875b73c
Matteo Gobbi <job@matteogobbi.it>
2015-04-18 13:56:46 +0100

    Tests customisation exposing private methods and properties in test class

--- SVProgressHUD Tests/SVProgressHUD_Customization_Tests.m
+++ SVProgressHUD Tests/SVProgressHUD_Customization_Tests.m
@@ -8,14 +8,35 @@
 
 #import <UIKit/UIKit.h>
 #import <Kiwi/Kiwi.h>
+#import "SVProgressHUD.h"
+
+
+@interface SVProgressHUD (ExposePrivate)
+
+@property (nonatomic, strong) UIControl *overlayView;
+@property (nonatomic, strong) UIView *hudView;
+@property (nonatomic, strong) UILabel *stringLabel;
+@property (nonatomic, strong) UIImageView *imageView;
+@property (nonatomic, strong) SVIndefiniteAnimatedView *indefiniteAnimatedView;
+
++ (SVProgressHUD*)sharedView;
+
+@end
+
 
 SPEC_BEGIN(SVProgressHUD_Customization_Tests)
 
 describe(@"SVProgressHUD - Customization methods", ^{
-    context(@"", ^{
+    let(progressHUD, ^{
+        [SVProgressHUD sharedView];
+    });
+    
+    context(@"setting the background color", ^{
+        
         it(@"", ^{
             
         });
+    
     });
 });
 
