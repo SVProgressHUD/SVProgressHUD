ca703ebd61ae32a60425ee054c80863a79ea1cab
Matteo Gobbi <job@matteogobbi.it>
2015-04-18 13:44:09 +0100

    Kiwi test for customisation

---
+++ SVProgressHUD Tests/SVProgressHUD_Customization_Tests.m
@@ -0,0 +1,22 @@
+//
+//  SVProgressHUD_Customization_Tests.m
+//  SVProgressHUD Tests
+//
+//  Created by Matteo Gobbi on 18/04/2015.
+//  Copyright (c) 2015 EmbeddedSources. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <Kiwi/Kiwi.h>
+
+SPEC_BEGIN(SVProgressHUD_Customization_Tests)
+
+describe(@"SVProgressHUD - Customization methods", ^{
+    context(@"", ^{
+        it(@"", ^{
+            
+        });
+    });
+});
+
+SPEC_END
