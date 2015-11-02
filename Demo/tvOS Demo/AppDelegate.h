070431b765ba9ccebf29f15ba840db89b975fbbf
David Olesch <david@jackrabbitmobile.com>
2015-11-02 17:13:57 -0600

    Added Segmented Controls. (+1 squashed commit) Squashed commits: [5b5f86a] Added a tvOS target to the Demo app. Added buttons for the seven main actions on ViewController.

---
+++ Demo/tvOS Demo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  tvOS Demo
+//
+//  Created by David Olesch on 11/2/15.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Demo/tvOS Demo/AppDelegate.m
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.m
+//  tvOS Demo
+//
+//  Created by David Olesch on 11/2/15.
+//
+//
+
+#import "AppDelegate.h"
+
+@interface AppDelegate ()
+
+@end
+
+@implementation AppDelegate
+
+@end
