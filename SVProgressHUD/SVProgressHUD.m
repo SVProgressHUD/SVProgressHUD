f9595918dc59bb3994a1562cc84ade5f54784796
Tobias Tiemerding <tobias.tiemerding@uni-oldenburg.de>
2016-01-17 13:25:10 +0100

    Tried to fix #529

--- SVProgressHUD/SVProgressHUD.m
+++ SVProgressHUD/SVProgressHUD.m
@@ -24,6 +24,7 @@ NSString * const SVProgressHUDStatusUserInfoKey = @"SVProgressHUDStatusUserInfoK
 
 static const CGFloat SVProgressHUDParallaxDepthPoints = 10;
 static const CGFloat SVProgressHUDUndefinedProgress = -1;
+static const CGFloat SVProgressHUDDefaultAnimationDuration = 0.15;
 
 @interface SVProgressHUD ()
 
@@ -281,18 +282,18 @@ static const CGFloat SVProgressHUDUndefinedProgress = -1;
     }
 }
 
++ (void)dismiss{
+    [self dismissWithDelay:0];
+}
+
++ (void)dismissWithDelay:(NSTimeInterval)delay{
+    [SVProgressHUD dismissWithDuration:SVProgressHUDDefaultAnimationDuration delay:delay];
+}
+
 + (void)dismissWithDuration:(NSTimeInterval)duration delay:(NSTimeInterval)delay{
     if([self isVisible]){
         [[self sharedView] dismissWithDuration:duration delay:delay];
     }
-}
-
-+ (void)dismissWithDelay:(NSTimeInterval)delay{
-    [SVProgressHUD dismissWithDuration:0.15 delay:delay];
-}
-
-+ (void)dismiss{
-    [self dismissWithDelay:0];
 }
 
 
@@ -858,9 +859,9 @@ static const CGFloat SVProgressHUDUndefinedProgress = -1;
         }
         
         __weak SVProgressHUD *weakSelf = self;
-        [UIView animateWithDuration:0.15
+        [UIView animateWithDuration:SVProgressHUDDefaultAnimationDuration
                               delay:0
-                            options:UIViewAnimationOptionAllowUserInteraction | UIViewAnimationCurveEaseOut | UIViewAnimationOptionBeginFromCurrentState
+                            options:(UIViewAnimationOptions) (UIViewAnimationOptionAllowUserInteraction | UIViewAnimationCurveEaseOut | UIViewAnimationOptionBeginFromCurrentState)
                          animations:^{
                              __strong SVProgressHUD *strongSelf = weakSelf;
                              if(strongSelf){
@@ -937,70 +938,57 @@ static const CGFloat SVProgressHUDUndefinedProgress = -1;
     
     self.activityCount = 0;
     __weak SVProgressHUD *weakSelf = self;
-    void (^animationBlock) (void) = ^{
-        __strong SVProgressHUD *strongSelf = weakSelf;
-        if(strongSelf){
-            strongSelf.hudView.transform = CGAffineTransformScale(self.hudView.transform, 0.8f, 0.8f);
-            if(strongSelf.isClear){ // handle iOS 7 UIToolbar not answer well to hierarchy opacity change
-                strongSelf.hudView.alpha = 0.0f;
-            } else{
-                strongSelf.alpha = 0.0f;
-            }
-        }
-    };
-    void (^completionBlock) (void) = ^{
-        __strong SVProgressHUD *strongSelf = weakSelf;
-        if(strongSelf){
-            if(strongSelf.alpha == 0.0f || strongSelf.hudView.alpha == 0.0f){
-                strongSelf.alpha = 0.0f;
-                strongSelf.hudView.alpha = 0.0f;
-                
-                [[NSNotificationCenter defaultCenter] removeObserver:strongSelf];
-                [strongSelf cancelRingLayerAnimation];
-                [_hudView removeFromSuperview];
-                _hudView = nil;
-                
-                [_overlayView removeFromSuperview];
-                _overlayView = nil;
-                
-                [_indefiniteAnimatedView removeFromSuperview];
-                _indefiniteAnimatedView = nil;
-                
-                UIAccessibilityPostNotification(UIAccessibilityScreenChangedNotification, nil);
-                
-                [[NSNotificationCenter defaultCenter] postNotificationName:SVProgressHUDDidDisappearNotification
-                                                                    object:nil
-                                                                  userInfo:userInfo];
-                
-                // Tell the rootViewController to update the StatusBar appearance
-#if !defined(SV_APP_EXTENSIONS) && TARGET_OS_IOS
-                UIViewController *rootController = [[UIApplication sharedApplication] keyWindow].rootViewController;
-                if([rootController respondsToSelector:@selector(setNeedsStatusBarAppearanceUpdate)]){
-                    [rootController setNeedsStatusBarAppearanceUpdate];
-                }
-#endif
-                // uncomment to make sure UIWindow is gone from app.windows
-                //NSLog(@"%@", [UIApplication sharedApplication].windows);
-                //NSLog(@"keyWindow = %@", [[[UIApplication sharedApplication] delegate] window]);
-            }
-        }
-    };
-    
-    if (duration == 0) {
-        animationBlock();
-        completionBlock();
-    }
-    else {
-        [UIView animateWithDuration:duration
-                              delay:delay
-                            options:(UIViewAnimationOptions) (UIViewAnimationCurveEaseIn | UIViewAnimationOptionAllowUserInteraction)
-                         animations:^{
-                             animationBlock();
+    [UIView animateWithDuration:duration
+                          delay:delay
+                        options:(UIViewAnimationOptions) (UIViewAnimationOptionAllowUserInteraction | UIViewAnimationCurveEaseIn | UIViewAnimationOptionBeginFromCurrentState)
+                     animations:^{
+                         __strong SVProgressHUD *strongSelf = weakSelf;
+                         if(strongSelf){
+                             strongSelf.hudView.transform = CGAffineTransformScale(self.hudView.transform, 0.8f, 0.8f);
+                             if(strongSelf.isClear){ // handle iOS 7 UIToolbar not answer well to hierarchy opacity change
+                                 strongSelf.hudView.alpha = 0.0f;
+                             } else{
+                                 strongSelf.alpha = 0.0f;
+                             }
                          }
-                         completion:^(BOOL finished){
-                             completionBlock();
-                         }];
-    }
+                     }
+                     completion:^(BOOL finished){
+                         __strong SVProgressHUD *strongSelf = weakSelf;
+                         if(strongSelf){
+                             if(strongSelf.alpha == 0.0f || strongSelf.hudView.alpha == 0.0f){
+                                 strongSelf.alpha = 0.0f;
+                                 strongSelf.hudView.alpha = 0.0f;
+                                 
+                                 [[NSNotificationCenter defaultCenter] removeObserver:strongSelf];
+                                 [strongSelf cancelRingLayerAnimation];
+                                 [_hudView removeFromSuperview];
+                                 _hudView = nil;
+                                 
+                                 [_overlayView removeFromSuperview];
+                                 _overlayView = nil;
+                                 
+                                 [_indefiniteAnimatedView removeFromSuperview];
+                                 _indefiniteAnimatedView = nil;
+                                 
+                                 UIAccessibilityPostNotification(UIAccessibilityScreenChangedNotification, nil);
+                                 
+                                 [[NSNotificationCenter defaultCenter] postNotificationName:SVProgressHUDDidDisappearNotification
+                                                                                     object:nil
+                                                                                   userInfo:userInfo];
+                                 
+                                 // Tell the rootViewController to update the StatusBar appearance
+#if !defined(SV_APP_EXTENSIONS) && TARGET_OS_IOS
+                                 UIViewController *rootController = [[UIApplication sharedApplication] keyWindow].rootViewController;
+                                 if([rootController respondsToSelector:@selector(setNeedsStatusBarAppearanceUpdate)]){
+                                     [rootController setNeedsStatusBarAppearanceUpdate];
+                                 }
+#endif
+                                 // uncomment to make sure UIWindow is gone from app.windows
+                                 //NSLog(@"%@", [UIApplication sharedApplication].windows);
+                                 //NSLog(@"keyWindow = %@", [[[UIApplication sharedApplication] delegate] window]);
+                             }
+                         }
+                     }];
 }
 
 - (void)dismiss
