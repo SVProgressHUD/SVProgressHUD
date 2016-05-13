cd61f0c66642c3b553d12c4484d6d7edd01a2a8f
Wolfire <wolfire_jiao@163.com>
2016-05-13 11:22:52 +0800

    Add multi-window based app support

--- SVProgressHUD/SVProgressHUD.h
+++ SVProgressHUD/SVProgressHUD.h
@@ -69,6 +69,8 @@ typedef NS_ENUM(NSUInteger, SVProgressHUDAnimationType) {
 @property (assign, nonatomic) NSTimeInterval fadeInAnimationDuration;  // default is 0.15
 @property (assign, nonatomic) NSTimeInterval fadeOutAnimationDuration; // default is 0.15
 
+@property (assign, nonatomic) UIWindowLevel windowLevel;                // the lowest window level, default is UIWindowLevelNormal
+
 
 + (void)setDefaultStyle:(SVProgressHUDStyle)style;                  // default is SVProgressHUDStyleLight
 + (void)setDefaultMaskType:(SVProgressHUDMaskType)maskType;         // default is SVProgressHUDMaskTypeNone
