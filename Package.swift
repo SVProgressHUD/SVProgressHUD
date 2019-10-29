ba33df746f3af2cc314fcee18df10700d5f0e281
Simone Gasparini <simone.gasparini@dualb.it>
2019-10-29 14:38:46 +0100

    Added Package.swift

--- Package.swift
+++ Package.swift
@@ -1,8 +1,28 @@
 // swift-tools-version:5.0
 // The swift-tools-version declares the minimum version of Swift required to build this package.
+
 import PackageDescription
 
 let package = Package(
     name: "SVProgressHUD",
+    platforms: [
+        .iOS(.v10)
+    ],
+    products: [
+        .library(
+            name: "SVProgressHUD",
+            targets: ["SVProgressHUD"]
+        )
+    ],
+    targets: [
+        .target(
+            name: "SVProgressHUD",
+            path: "SVProgressHUD",
+            exclude: [
+                "SVProgressHUD-Framework",
+                "Demo"
+            ]
+        ),
+    ],
     swiftLanguageVersions: [.v5]
 )
